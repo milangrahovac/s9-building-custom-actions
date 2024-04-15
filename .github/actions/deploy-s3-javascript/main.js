ce30f6bf821b273f1a78e3a09f0f4e49a0ad9fa5
milan <grahovac85@gmail.com>
2024-04-15 14:35:10 +0200

    added access keys

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,6 +4,16 @@ const exec = require('@actions/exec')
 
 
 function run() {
+    // 1) Get some input values
+    const bucket = core.getInput('bucket', { requred: true });
+    const bucketRegion = core.getInput('bucket-region', { requred: true });
+    const distFolder = core.getInput('dist-folder', { requred: true });
+
+    // 2) Upload my files.
+    const s3Url = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Url} --region ${bucketRegion} `)
+
+
     core.notice('Hellow World!')
 }
 
